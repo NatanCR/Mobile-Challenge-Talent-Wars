d171181cd0ec7beb915e09c9953c02a8f369e925
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2024-02-20 16:02:38 +1000

    feat: custom search bar

--- Talent-Wars/Talent-Wars/Views/HomeViewController.swift
+++ Talent-Wars/Talent-Wars/Views/HomeViewController.swift
@@ -7,30 +7,18 @@
 
 import UIKit
 
-class HomeViewController: UIViewController, UISearchBarDelegate {
+class HomeViewController: UIViewController, UITextFieldDelegate {
     var coordinator: Coordinator?
     
-    private let searchBar = UISearchBar()
     private let tableView = UITableView()
+    private let titleView = UILabel()
+    private let greenBackgroundView = UIView()
+    private var searchBar = UITextField()
     
     var viewModel = HomeViewModel() // Instantiating the ViewModel
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Setting the view's title.
-        title = "Popular Right now"
-        
-        let appearance = UINavigationBarAppearance()
-        appearance.configureWithOpaqueBackground()
-        appearance.backgroundColor = UIColor(named: "homeBG") // The background color
-        appearance.titleTextAttributes = [
-            .font: UIFont(name: "Jomhuria-Regular", size: 40) ?? UIFont.systemFont(ofSize: 40), .foregroundColor: UIColor(named: "homeTitle") ?? UIColor.white
-        ]
-        
-        navigationController?.navigationBar.standardAppearance = appearance
-        navigationController?.navigationBar.scrollEdgeAppearance = appearance
-        navigationController?.navigationBar.compactAppearance = appearance
         
         tableView.separatorStyle = .none
         searchBar.delegate = self
@@ -43,36 +31,66 @@ class HomeViewController: UIViewController, UISearchBarDelegate {
         
         viewModel.fetchMoviesFromAPI()
         setupTableView()
-        setupSearchBar()
         setupLayout()
+        setupSearchTextField()
+        setupTitleLabel()
         bindViewModel()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
+        navigationController?.navigationBar.isHidden = true
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        navigationController?.navigationBar.isHidden = false
-    }
-    
-    // Implementation of UISearchBarDelegate
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    // Implement UITextFieldDelegate methods
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let searchText = (textField.text! as NSString).replacingCharacters(in: range, with: string)
         viewModel.filterMovies(with: searchText)
         
         if searchText.isEmpty {
-            title = "Popular Right now" // Title for the full list
+            titleView.text = "Popular Right now"
         } else {
-            title = "Your Results" // Title for the search results
+            titleView.text = "Your Results"
         }
+        
+        return true
     }
     
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.text = ""
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        textField.text = ""
         viewModel.filterMovies(with: "")
-        title = "Popular Right now" // Restore the original title
-        searchBar.resignFirstResponder() // Hides the keyboard
+        titleView.text = "Popular Right now"
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    private func setupSearchTextField() {
+        let placeholderAttributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.black, // Change to your desired placeholder color
+            .font: UIFont.boldSystemFont(ofSize: 17) // You can also set the font here if needed
+        ]
+        
+        // Set the attributedPlaceholder property with the NSAttributedString
+        searchBar.attributedPlaceholder = NSAttributedString(string: "Search", attributes: placeholderAttributes)
+        searchBar.borderStyle = .none
+        searchBar.backgroundColor = UIColor.white
+        searchBar.clearButtonMode = .whileEditing
+        searchBar.returnKeyType = .search
+        
+        searchBar.layer.cornerRadius = 26
+        searchBar.layer.masksToBounds = true
+        
+        // Set the font and text alignment
+        searchBar.font = UIFont.boldSystemFont(ofSize: 17)
+        searchBar.textAlignment = .left
+        
+        // Set padding for the left side of the text field
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 15, height: searchBar.frame.height))
+        searchBar.leftView = paddingView
+        searchBar.leftViewMode = .always
+        
+        // Set the clear button
+        searchBar.clearButtonMode = .whileEditing
     }
     
     private func bindViewModel() {
@@ -89,28 +107,52 @@ class HomeViewController: UIViewController, UISearchBarDelegate {
         tableView.delegate = self
     }
     
-    private func setupSearchBar() {
-        searchBar.placeholder = "Search"
-    }
-    
     private func setupLayout() {
         
-        view.addSubview(searchBar)
+        view.addSubview(greenBackgroundView)
         view.addSubview(tableView)
+        view.addSubview(searchBar)
+        
         
         searchBar.translatesAutoresizingMaskIntoConstraints = false
+        greenBackgroundView.translatesAutoresizingMaskIntoConstraints = false
         tableView.translatesAutoresizingMaskIntoConstraints = false
+        
         tableView.backgroundColor = .white
+        greenBackgroundView.backgroundColor = UIColor(named: "homeBG")
         
         NSLayoutConstraint.activate([
             searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            searchBar.heightAnchor.constraint(equalToConstant: 50),
             
-            tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor),
+            // Green background view constraints
+            greenBackgroundView.topAnchor.constraint(equalTo: view.topAnchor),
+            greenBackgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            greenBackgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            greenBackgroundView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.3),
+            
+            tableView.topAnchor.constraint(equalTo: greenBackgroundView.topAnchor, constant: 250),
             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    private func setupTitleLabel() {
+        titleView.text = "Popular Right now"
+        titleView.textAlignment = .center
+        titleView.font = UIFont(name: "Jomhuria-Regular", size: 60)
+        titleView.textColor = UIColor(named: "homeTitle")
+        
+        view.addSubview(titleView)
+        titleView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 70),
+            titleView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            titleView.heightAnchor.constraint(equalToConstant: 60)
         ])
     }
 }
